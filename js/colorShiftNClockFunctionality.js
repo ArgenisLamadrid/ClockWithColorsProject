4009d760906b1a87dff5723df4171a720b8d3286
Argenis <argenislamadrid@gmail.com>
2018-10-08 12:05:26 -0700

    used prettier

--- js/colorShiftNClockFunctionality.js
+++ js/colorShiftNClockFunctionality.js
@@ -9,53 +9,52 @@ var colorTicker = document.getElementById("colorNumber");
 var clockTicker = document.getElementById("clock");
 
 clockUpdate();
-colorShiftUpdate(hours,minutes,seconds);
-    
+colorShiftUpdate(hours, minutes, seconds);
 
-function colorShiftUpdate(r,g,b){
-    /* Updates the background color and color ticker. Input is 3 regular decimal
+function colorShiftUpdate(r, g, b) {
+  /* Updates the background color and color ticker. Input is 3 regular decimal
     numbers gathered from the clock ticker */
-    r = clockToRGB(r,24);
-    g = clockToRGB(g,60);
-    b = clockToRGB(b,60);
-    // converts numbers to 0-255 range
+  r = clockToRGB(r, 24);
+  g = clockToRGB(g, 60);
+  b = clockToRGB(b, 60);
+  // converts numbers to 0-255 range
 
-   var rHex = format(r.toString(16));
-   var gHex = format(g.toString(16));
-   var bHex = format(b.toString(16));
-   // converts to hex
-    background.style.backgroundColor = "#" + rHex + gHex + bHex;
-    colorTicker.innerHTML = (rHex + ":" + gHex + ":" + bHex);
-    setTimeout('colorShiftUpdate(hours,minutes,seconds)',1);
+  var rHex = format(r.toString(16));
+  var gHex = format(g.toString(16));
+  var bHex = format(b.toString(16));
+  // converts to hex
+  background.style.backgroundColor = "#" + rHex + gHex + bHex;
+  colorTicker.innerHTML = rHex + ":" + gHex + ":" + bHex;
+  setTimeout("colorShiftUpdate(hours,minutes,seconds)", 1);
 }
 
-function clockUpdate(){
-/* Updates, displays, manages clock  */
-    currentTime = new Date();
-    hours = currentTime.getHours();
-    minutes = currentTime.getMinutes(); 
-    seconds = currentTime.getSeconds();
-// updating clock txt clock
-     clockTicker.innerHTML= (format(hours) + ":" + format(minutes) + ":" + format(seconds));
-    setTimeout('clockUpdate()',1);
+function clockUpdate() {
+  /* Updates, displays, manages clock  */
+  currentTime = new Date();
+  hours = currentTime.getHours();
+  minutes = currentTime.getMinutes();
+  seconds = currentTime.getSeconds();
+  // updating clock txt clock
+  clockTicker.innerHTML =
+    format(hours) + ":" + format(minutes) + ":" + format(seconds);
+  setTimeout("clockUpdate()", 1);
 }
 
-function clockToRGB(num,setting){
-    // Converts clock numbers, hours and minutes to a 0-255 number.
-    if(setting == "24"){
-        return Math.round((num/23) * 255);
-    }else if(setting == "60"){
-        return Math.round((num/59) * 255);
-    }
-    return num;
+function clockToRGB(num, setting) {
+  // Converts clock numbers, hours and minutes to a 0-255 number.
+  if (setting == "24") {
+    return Math.round((num / 23) * 255);
+  } else if (setting == "60") {
+    return Math.round((num / 59) * 255);
+  }
+  return num;
 }
 
-function format(num){
-    // formats numbers to be displayed, if its less than 2 places long, 
-    // adds a 0
-    if(num.length < 2){
-        return "0" + num;
-    } 
-    return num;
+function format(num) {
+  // formats numbers to be displayed, if its less than 2 places long,
+  // adds a 0
+  if (num.length < 2) {
+    return "0" + num;
+  }
+  return num;
 }
-
--- js/modalBoxMeme.js
+++ js/modalBoxMeme.js
@@ -1,33 +1,35 @@
- //Incomplete
- var screenHeight = $(window).height();
- var screenWidth = $(window).height();
- var layerHeight =  $("#colorLayer").height();
- var layerWidth =  $("#colorLayer").width();
- var trapModalHeight = $("#trapModal").height();
- var trapModalWidth = $("#trapModal").width();
+//Incomplete
+var screenHeight = $(window).height();
+var screenWidth = $(window).height();
+var layerHeight = $("#colorLayer").height();
+var layerWidth = $("#colorLayer").width();
+var trapModalHeight = $("#trapModal").height();
+var trapModalWidth = $("#trapModal").width();
 
 var trapButton = document.getElementById("trapButton");
 var closeModalButton = document.getElementById("closeModalButton");
 var trapModal = document.getElementsByClassName("modal")[0];
 
+trapButton.addEventListener("click", function() {
+  openModal();
+});
+closeModalButton.addEventListener("click", function() {
+  repositionModal();
+});
 
-trapButton.addEventListener("click",function() {openModal();});
-closeModalButton.addEventListener("click",function() {repositionModal();});
+function repositionModal() {
+  screenHeight = $(window).height();
+  screenWidth = $(window).width();
 
-function repositionModal(){
-    screenHeight = $(window).height();
-    screenWidth = $(window).width();
+  console.log(layerHeight + " layerHeight");
+  console.log(layerWidth + " layerWidth");
+  console.log(trapModalHeight + " modalHeight");
+  console.log(trapModalWidth + " modalWidth");
 
-    console.log(layerHeight + " layerHeight");
-    console.log(layerWidth + " layerWidth");
-    console.log(trapModalHeight + " modalHeight");
-    console.log(trapModalWidth + " modalWidth");
-
-    trapModal.style.top = ((Math.random() * layerHeight) ) + "px";
-    trapModal.style.left = ((Math.random() * layerWidth) ) + "px";
-
+  trapModal.style.top = Math.random() * layerHeight + "px";
+  trapModal.style.left = Math.random() * layerWidth + "px";
 }
 
-function openModal(){
-    trapModal.style.display = "block";
+function openModal() {
+  trapModal.style.display = "block";
 }
